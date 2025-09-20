a892404fbe85dbb508e4317b14797fd5da61867c
Joel J <joeljustin259200@gmail.com>
2025-09-20 07:37:34 +0000

    just make the loging with email alone ,no google signin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -52,9 +52,6 @@ export default function LoginPage() {
             <Button type="submit" className="w-full" asChild>
               <Link href="/dashboard">Login</Link>
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
           </div>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{' '}
