b6e3415d0f1234d498057a51236b7531bf59c780
Joel J <joeljustin259200@gmail.com>
2025-09-20 08:19:51 +0000

    still ist showing the loading

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -55,7 +55,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 };
